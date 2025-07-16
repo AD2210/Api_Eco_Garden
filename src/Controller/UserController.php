d8d64be4b2bc6b0411c6aa49a01c06d8a30377c8
Anthony Delbee <anthony.delbee@gmail.com>
2025-07-16 09:32:12 +0200

    Gestion des hash password pour Post et Put

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,14 +12,19 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route(path: "/api", name: "app_api_")]
 final class UserController extends AbstractController
 {
+    private $passwordHasher;
+    public function __construct(UserPasswordHasherInterface $passwordHasher) {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     #[Route('/user', name: 'user_create', methods:['POST'])]
     public function createUser(Request $request, EntityManagerInterface $em, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
     {
-        //On extrait la requetes et on dérialise en une instance de User (role = [] => ROLE_USER dans le getter)
         $user = $serializer->deserialize($request->getContent(), User::class, 'json');
 
         //On contrôle les erreurs du validator paramétré dans l'entité (Assert)
@@ -28,7 +33,12 @@ final class UserController extends AbstractController
             return new JsonResponse($serializer->serialize($error, 'json'), Response::HTTP_BAD_REQUEST, [], true);
         }
 
+        //On hash le password pour sauvegarde bdd
+        $plainPassword = $user->getPassword();
+        $user->setPassword($this->passwordHasher->hashPassword($user, $plainPassword));
+
         $user->setRoles(["ROLE_USER"]); // On force ROLE_USER à la création pour eviter que n'importe qui soit Admin
+
         //On persist et sauvegarde en bdd
         $em->persist($user);
         $em->flush();
@@ -50,6 +60,13 @@ final class UserController extends AbstractController
         if ($error->count() > 0) {
             return new JsonResponse($serializer->serialize($error, 'json'), Response::HTTP_BAD_REQUEST, [], true);
         }
+
+        //On vérifie la présence de la maj du password et on hash. cela evite de hashé 2 fois
+        if ($serializer->deserialize($request->getContent(), User::class, 'json')->getPassword() !== null){
+            $plainPassword = $updatedUser->getPassword();
+            $updatedUser->setPassword($this->passwordHasher->hashPassword($updatedUser, $plainPassword));
+        }
+
         //On persist et sauvegarde en bdd le conseil mis à jour
         $em->persist($updatedUser);
         $em->flush();
